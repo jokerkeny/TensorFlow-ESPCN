6f9df85a5514216934867c703252460b407ebaac
YangYang <jokerkeny@gmail.com>
2018-11-19 15:34:02 -0500

    integrate the rebuild for loop in espcn

--- espcn.py
+++ espcn.py
@@ -6,24 +6,31 @@ import time
 import os
 import h5py
 import numpy as np
+import prepare_data
+import util
 
+
+TEST_IMAGE_DIR = './test_images/'
+TEST_RESULT_DIR = './result/'
 
 class ESPCN:
-    def __init__(self, sess, is_train, image_height, image_width, image_channel, ratio):
+    def __init__(self, sess, is_train,image_channel, ratio, image_height=prepare_data.IMAGE_SIZE, image_width=prepare_data.IMAGE_SIZE):
         self.sess = sess
-        self.image_height = image_height
-        self.image_width = image_width
+        # self.image_height = image_height
+        # self.image_width = image_width
         self.image_channel = image_channel
         self.ratio = ratio
         self.is_train = is_train
 
+        self.image_height=image_height
+        self.image_width=image_width
         self.batch_size = 100
         self.learning_rate = 0.00001
         self.epoch = 100
         self.checkpoint_dir = 'checkpoint'
         self.train_data_path = 'h5data' + '/' + 'train_data.h5'
 
-        self.create_network()
+        # self.create_network()
 
     def create_network(self):
         """
@@ -217,15 +224,54 @@ class ESPCN:
                 if step % 500 == 0:
                     self.save(step)
 
-    def generate(self, lr_image):
+    def generate(self, img_list):
         """
         生成高分辨率图
         :param lr_image:
         :return:
         """
+
+        self.create_network()
         self.load_checkpoint()
-        result = self.pred.eval({self.images: lr_image.reshape(
-            1, self.image_height, self.image_width, self.image_channel)})
-        result[result < 0] = 0
-        sr_image = np.squeeze(result) * 255.
-        return np.uint8(sr_image)
+
+        for img_name in img_list:
+            lr_image, ori_image = prepare_data.preprocess_img(os.path.join(TEST_IMAGE_DIR,img_name))
+            try:
+                image_height, image_width, _ = lr_image.shape
+            except:
+                return
+            
+            lr_image2=lr_image
+            lr_image=lr_image/255.0
+            self.image_height = image_height
+            self.image_width = image_width
+
+
+            self.create_network()
+
+            result = self.pred.eval({self.images: lr_image.reshape(
+                1, self.image_height, self.image_width, self.image_channel)})
+            result[result < 0] = 0
+            sr_image = np.squeeze(result) * 255.
+
+            
+            sr_image = np.uint8(sr_image)
+
+            # otherwise there would be error for image.save
+            if not os.path.isdir(TEST_RESULT_DIR):
+                os.makedirs(TEST_RESULT_DIR)
+
+            # lr image
+            # util.show_img_from_array(lr_image)
+            lr_image=lr_image2
+            util.save_img_from_array(
+                lr_image, TEST_RESULT_DIR+img_name.split('.')[0]+'_lr.png')
+            # original image
+            # util.show_img_from_array(ori_image)
+            util.save_img_from_array(ori_image, TEST_RESULT_DIR +
+                                    img_name.split('.')[0]+'_hr.png')
+            # sr image
+            # util.show_img_from_array(sr_image)
+            util.save_img_from_array(sr_image, TEST_RESULT_DIR +
+                                    img_name.split('.')[0]+'_sr.png')
+
--- rebuild.py
+++ rebuild.py
@@ -1,54 +1,30 @@
 import tensorflow as tf
 import os
 
-from espcn import ESPCN
 import prepare_data
-import util
+from espcn import ESPCN
+
 
 TEST_IMAGE_DIR = './test_images/'
 TEST_RESULT_DIR = './result/'
 
 
-def rebuild(img_name):
+def rebuild(img_list):
     """
     图像超分辨率重建
     :return:
     """
-    lr_image, ori_image = prepare_data.preprocess_img(os.path.join(TEST_IMAGE_DIR,img_name))
-    try:
-        image_height, image_width, _ = lr_image.shape
-    except:
-        return
+    
     with tf.Session() as sess:
         espcn = ESPCN(sess,
                       is_train=False,
-                      image_height=image_height,
-                      image_width=image_width,
+                    #   image_height=image_height,
+                    #   image_width=image_width,
                       image_channel=prepare_data.IMAGE_CHANNEl,
                       ratio=prepare_data.RATIO)
-        sr_image = espcn.generate(lr_image / 255.0)
-
-    
-    # otherwise there would be error for image.save
-    if not os.path.isdir(TEST_RESULT_DIR):
-        os.makedirs(TEST_RESULT_DIR)
-
-    # lr image
-    # util.show_img_from_array(lr_image)
-    util.save_img_from_array(
-        lr_image, TEST_RESULT_DIR+img_name.split('.')[0]+'_lr.png')
-    # original image
-    # util.show_img_from_array(ori_image)
-    util.save_img_from_array(ori_image, TEST_RESULT_DIR +
-                             img_name.split('.')[0]+'_hr.png')
-    # sr image
-    # util.show_img_from_array(sr_image)
-    util.save_img_from_array(sr_image, TEST_RESULT_DIR +
-                             img_name.split('.')[0]+'_sr.png')
-
+        espcn.generate(img_list)
 
 if __name__ == '__main__':
     img_list = [filename for filename in os.listdir(
         TEST_IMAGE_DIR) if filename.endswith('jpg')]
-    for filename in img_list:
-        rebuild(filename)
+    rebuild(img_list)
